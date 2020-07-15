3e39f24e868937006bb5fbdc777d28ac7e99a1dd
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-07-15 17:37:00 -0600

    added function to favorite a sneaker

--- js/services/API.js
+++ js/services/API.js
@@ -40,6 +40,20 @@ class API {
         })
     }
 
-
+    static favoriteSneaker() {
+        const fav = parseInt(event.target.parentElement.querySelector("p").innerText.split(" ")[0])
+        let updateFavs = fav + 1 
+        event.target.parentElement.querySelector("p").innerText = `${updateFavs} favorites`
+        const id = parseInt(event.target.parentElement.id)
+        fetch(`http://localhost:3000/sneakers/${id}`,{
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            favorite: updateFavs 
+          })
+      })
+    }
 
 }
