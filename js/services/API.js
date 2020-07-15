c14768d97122e74e2f311b0676175cac96260762
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-07-15 17:37:37 -0600

    added function to cop a sneaker

--- js/services/API.js
+++ js/services/API.js
@@ -56,4 +56,20 @@ class API {
       })
     }
 
+    static copSneaker() {
+        const cop = parseInt(event.target.parentElement.querySelector("p").innerText.split(" ")[0])
+        let updateCops = cop + 1 
+        event.target.parentElement.querySelector("p").innerText = `${updateCops} cops`
+        const id = parseInt(event.target.parentElement.id)
+        fetch(`http://localhost:3000/sneakers/${id}`,{
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            cop: updateCops
+          })
+      })
+    }
+
 }
