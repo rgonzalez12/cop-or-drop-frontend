e674304515d4d4b35333bd204ddb27a5ea6e511a
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-07-15 16:57:38 -0600

    built sneaker class skeleton and constructor

---
+++ js/models/sneaker.js
@@ -0,0 +1,19 @@
+class Sneaker{
+    constructor(id, manufacturer, model, colorway, size, condition, est_value, image, favorite, cop, drop, opinions) {
+        this.id = id
+        this.manufacturer = manufacturer
+        this.model = model
+        this.colorway = colorway
+        this.size = size
+        this.condition = condition
+        this.est_value = est_value
+        this.image = image
+        this.favorite = favorite
+        this.cop = cop
+        this.drop = drop
+        this.opinions = opinions
+        this.renderSneaker()
+    }
+
+    
+}
