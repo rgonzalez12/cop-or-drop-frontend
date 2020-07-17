a884ac042a8e8fec44a59282b9d949d4ba896dc5
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-07-17 11:30:21 -0600

    added refactoring from project review

--- js/models/sneaker.js
+++ js/models/sneaker.js
@@ -24,6 +24,7 @@ class Sneaker {
         sneakerContainer.appendChild(sneakerCard)
         sneakerCard.addEventListener('click', e => {
           if (e.target.className.includes('delete')) this.deleteSneaker(e)
+          if (e.target.className.includes('update')) this.updateSneaker(e)
         })
     }
 
@@ -40,6 +41,22 @@ class Sneaker {
       }) 
     }
 
+    updateSneaker(event) {
+      const sneakerCondition = event.target.parentElement.querySelector(".update").innerText
+      const sneakerId = parseInt(event.target.parentElement.id)
+      fetch(`http://localhost:3000/sneakers/${sneakerId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify('used')
+      })
+      .then (updatedCondition => updatedCondition.json())
+      .then (newestCondition => {
+        document.getElementById(sneakerCondition).innerText(newestCondition)
+      })
+    }
+
     sneakerHTML(){
         return `
           <h2 class="header">${this.model}</h2>
@@ -47,7 +64,7 @@ class Sneaker {
           <h5>Manufacturer: ${this.manufacturer}</h5>
           <h5>Colorway: ${this.colorway}</h5>
           <h5>Size: ${this.size}</h5>
-          <h5>Condition: ${this.condition}</h5>
+          <h5 class="condition">Condition: ${this.condition}</h5>
           <h5>Estimated Value: $ ${this.est_value}</h5>
           <p class="favorite"> ${this.favorite} favorites </p>
           <p class="cop"> ${this.cop} cops </p>
@@ -55,6 +72,7 @@ class Sneaker {
           <button onclick=API.favoriteSneaker()> Favorite </button>
           <button onclick=API.copSneaker()> Cop </button>
           <button onclick=API.dropSneaker()> Drop </button>
+          <button class= 'update'> Update Condition </button>
           <button class= 'delete'> Delete? </button>
           <h5>Summary: </h5>
           ${this.opinions.map(function(opinion){
